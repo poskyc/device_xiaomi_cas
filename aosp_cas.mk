74b1bf16844132257c346b8ad859dca09e602ca1
poskyc <poskyc@outlook.com>
2022-01-23 13:50:00 +0800

    pixelplusui ify

--- aosp_cas.mk
+++ aosp_cas.mk
@@ -6,14 +6,19 @@
 
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_p.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit from the device configuration.
 $(call inherit-product, device/xiaomi/cas/device.mk)
 
 # Inherit from the Aosp configuration.
 $(call inherit-product, vendor/aosp/config/common_full_phone.mk)
+TARGET_BOOT_ANIMATION_RES := 1080
+TARGET_INCLUDE_LIVE_WALLPAPERS := false
+
+#PixelPlus-UI Official Stuff
+CUSTOM_BUILD_TYPE := UNOFFICIAL
+PPUI_MAINTAINER := poskyc
 
 PRODUCT_NAME := aosp_cas
 PRODUCT_DEVICE := cas
@@ -21,8 +26,6 @@ PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := M2007J1SC
 PRODUCT_MANUFACTURER := Xiaomi
 
-DEVICE_MAINTAINER := poskyc
-
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
-BUILD_FINGERPRINT := Xiaomi/cas/cas:12/SKQ1.211006.001/21.12.8:user/release-keys
+BUILD_FINGERPRINT := Xiaomi/cas/cas:11/RKQ1.200826.002/V12.5.6.0.RJJCNXM:user/release-key
